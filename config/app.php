581c1fb8cc8950dcba63056778184c858ce599f7
Phan Ngoc Tuan <ngoctuan@TuanPhan.local>
2020-02-28 10:46:29 +0700

    Kafka producer and settings

--- config/app.php
+++ config/app.php
@@ -175,6 +175,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // Kafka service providers
+        App\Providers\ProducerServiceProvider::class,
     ],
 
     /*
